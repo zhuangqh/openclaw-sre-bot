d8ad6d855f67bbe2d67413db6783eeaad019dd2e
zhuangqh <zhuangqhc@gmail.com>
2026-02-08 16:17:15 +1100

    init project
    Signed-off-by: zhuangqh <zhuangqhc@gmail.com>

---
+++ deploy/chart/files/init-script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+echo "Initializing openclaw home directory..."
+
+OPENCLAW_HOME=/home/kasm-user/.openclaw
+OPENCLAW_WORKSPACE=/home/kasm-user/clawd
+
+# Copy config file
+echo "Copying openclaw configuration..."
+cp /tmp/config/openclaw.json $OPENCLAW_HOME/openclaw.json
+
+# Fix permissions on the PVC mount
+echo "Setting ownership to 1000:1000..."
+chown -R 1000:1000 $OPENCLAW_HOME
+chown -R 1000:1000 $OPENCLAW_WORKSPACE
+
+echo "Setting permissions to 755..."
+chmod -R 755 $OPENCLAW_HOME
+chmod -R 755 $OPENCLAW_WORKSPACE
+
+mkdir -p /etc/supervisor/conf.d
+cp /tmp/config/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+echo "Initialization complete!"
